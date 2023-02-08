643f505d71101ba5faa175ccf6d4eae6d352d3fd
SimonGideon <simongideon918@gmail.com>
2023-02-09 02:28:39 +0300

    Add linter files

--- morse.rb
+++ morse.rb
@@ -1,50 +1,50 @@
 $morse_code = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z',
+  ".-" => "A",
+  "-..." => "B",
+  "-.-." => "C",
+  "-.." => "D",
+  "." => "E",
+  "..-." => "F",
+  "--." => "G",
+  "...." => "H",
+  ".." => "I",
+  ".---" => "J",
+  "-.-" => "K",
+  ".-.." => "L",
+  "--" => "M",
+  "-." => "N",
+  "---" => "O",
+  ".--." => "P",
+  "--.-" => "Q",
+  ".-." => "R",
+  "..." => "S",
+  "-" => "T",
+  "..-" => "U",
+  "...-" => "V",
+  ".--" => "W",
+  "-..-" => "X",
+  "-.--" => "Y",
+  "--.." => "Z",
 }
 
-def decode_letter(code='')
-    print $morse_code[code]
+def decode_letter(code = "")
+  print $morse_code[code]
 end
 
 def decode_word(word)
-    _codearr = word.split
-    _res = ''
-    _codearr.each{|c| _res+= $morse_code[c]}
-    print _res + " "
+  codearr = word.split
+  res = ""
+  codearr.each { |c| res += $morse_code[c] }
+  print "#{res} "
 end
 
 def decode_phrase(code)
-    _codearr = code.split"   "
-    _codearr.each{|word| decode_word word}
+  codearr = code.split "   "
+  codearr.each { |word| decode_word word }
 end
 
-decode_letter('.-')
+decode_letter(".-")
 
-decode_word('-..- -.--')
+decode_word("-..- -.--")
 
-decode_phrase('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+decode_phrase(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
